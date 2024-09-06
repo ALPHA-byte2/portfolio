50c8adbf154b3e24fe9541d215f53ad772813b07
ALPHA-byte2 <josiahlogan434@gmail.com>
2024-09-06 20:22:28 +0300

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Analytics } from "@vercel/analytics/react"
+
 import React, { useState, useEffect } from "react";
 import Preloader from "../src/components/Pre";
 import Navbar from "./components/Navbar";
